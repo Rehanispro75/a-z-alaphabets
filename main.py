03d7b59c5c0229b65321b0ae955a7e5ae2dd817f
Rehanispro75 <rehanshaikh218053@gmail.com>
2024-12-10 18:52:35 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+input = input("enter a temprature ")
+
+if input > -16 and input  < 4 :
+    print("enter you should wear atleast 3 warm jakets ")
+elif input > 5 and input < 19 :
+    print("you should wear a sweater")
+elif input > 20 and input < 29 :
+    print("you should wear normal clothes ")
+elif input > 30 and input < 40 :
+    print("you should wear light clothes like sports clothes like jersey and all etc ")
+else :
+    print("type number between -15 and 50")
+ 
